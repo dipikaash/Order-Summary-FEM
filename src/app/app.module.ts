c1b30be07e2015243693da21da34c0fbafdf35eb
Abhirup Nandy <abhirupnandy.rocks@gmail.com>
2021-11-14 21:45:12 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderPageComponent } from './order-page/order-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderPageComponent
   ],
   imports: [
     BrowserModule
